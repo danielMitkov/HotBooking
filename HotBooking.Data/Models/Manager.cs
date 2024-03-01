109d025de805320b6081c6421dabbaa055387d79
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-01 11:27:21 +0200

    added foreign key attributes to manager entity

--- HotBooking.Data/Models/Manager.cs
+++ HotBooking.Data/Models/Manager.cs
@@ -1,10 +1,21 @@
-﻿namespace HotBooking.Data.Models;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotBooking.Data.Models;
 public class Manager
 {
     public int Id { get; set; }
-    public string UserId { get; set; }
 
+    [Required]
+    public string UserId { get; set; } = null!;
+
+    [ForeignKey(nameof(UserId))]
+    public IdentityUser User { get; set; } = null!;
+
+    [Required]
     public int HotelId { get; set; }
 
+    [ForeignKey(nameof(HotelId))]
     public Hotel Hotel { get; set; } = null!;
 }
