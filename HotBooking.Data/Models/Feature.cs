4f0f5561bbe515da53192763cae17383c448a392
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-02 20:42:20 +0200

    Feature entity data annotations added

--- HotBooking.Data/Models/Feature.cs
+++ HotBooking.Data/Models/Feature.cs
@@ -1,9 +1,14 @@
-﻿namespace HotBooking.Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotBooking.Data.Models;
 public class Feature
 {
+    [Key]
     public int Id { get; set; }
 
+    [Required]
     public string Name { get; set; } = null!;
 
+    [Required]
     public string IconClassName { get; set; } = null!;
 }
