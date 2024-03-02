a2d81d756cb8771c41dcfe7177ab422df3936793
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-02 20:47:04 +0200

    Facility entity removed description for now

--- HotBooking.Data/Models/Facility.cs
+++ HotBooking.Data/Models/Facility.cs
@@ -1,11 +1,14 @@
-﻿namespace HotBooking.Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotBooking.Data.Models;
 public class Facility
 {
+    [Key]
     public int Id { get; set; }
 
+    [Required]
     public string Name { get; set; } = null!;
 
+    [Required]
     public string IconClassName { get; set; } = null!;
-
-    public string Description { get; set; } = null!;
 }
