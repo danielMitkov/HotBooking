e71ebe631a73f327ac972472107cd8635951c2b9
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-02 20:33:05 +0200

    Review entity title added

--- HotBooking.Data/Models/Review.cs
+++ HotBooking.Data/Models/Review.cs
@@ -1,16 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HotBooking.Data.Models;
 public class Review
 {
     [Key]
     public int Id { get; set; }
-
-    [Required]
-    public int UserId { get; set; }
-
-    [Required]
-    public int RoomId { get; set; }
 
     [Required]
     public int Rating { get; set; }
@@ -24,7 +20,21 @@ public class Review
     [Required]
     public DateTime ReviewedOn { get; set; }
 
- 
+    [Required]
+    public int UserId { get; set; }
 
+    [ForeignKey(nameof(UserId))]
+    public IdentityUser User { get; set; } = null!;
 
+    [Required]
+    public int RoomId { get; set; }
+
+    [ForeignKey(nameof(RoomId))]
+    public Room Room { get; set; } = null!;
+
+    [Required]
+    public int BookingId { get; set; }
+
+    [ForeignKey(nameof(BookingId))]
+    public Booking Booking { get; set; } = null!;
 }
