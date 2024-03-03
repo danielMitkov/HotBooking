86b3252571df2a92e8733392109677b7ed1c7dca
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-03 14:05:00 +0200

    Create RoomConstants.cs

---
+++ HotBooking.Data/Constants/RoomConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace HotBooking.Data.Constants;
+public static class RoomConstants
+{
+    public const int TitleLengthMin = 5;
+    public const int TitleLengthMax = 50;
+
+    public const int DescriptionLengthMin = 10;
+    public const int DescriptionLengthMax = 1000;
+
+    public const int BedsCountMin = 1;
+    public const int BedsCountMax = 100;
+
+    public const int RoomSizeSquareMetersMin = 5;
+    public const int RoomSizeSquareMetersMax = 500;
+
+    public const int PricePerNightMin = 1;
+    public const int PricePerNightMax = 100000;
+}
