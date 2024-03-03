edfea0f8a62ccfc4288cc2f93e93f32a8f3a4d2b
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-03 11:20:54 +0200

    Booking entity dates added

--- HotBooking.Data/Models/Booking.cs
+++ HotBooking.Data/Models/Booking.cs
@@ -1,5 +1,31 @@
-﻿namespace HotBooking.Data.Models;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotBooking.Data.Models;
 public class Booking
 {
+    [Key]
+    public int Id { get; set; }
 
+    [Required]
+    public DateTime CheckIn { get; set; }
+
+    [Required]
+    public DateTime CheckOut { get; set; }
+
+    [Required]
+    public int AdultsCount { get; set; }
+
+    [Required]
+    public int RoomId { get; set; }
+
+    [ForeignKey(nameof(RoomId))]
+    public Room Room { get; set; } = null!;
+
+    [Required]
+    public int UserId { get; set; }
+
+    [ForeignKey(nameof(UserId))]
+    public IdentityUser User { get; set; } = null!;
 }
