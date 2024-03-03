692665da9bff49756f12d6524830fb6233ce5374
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-03 13:25:47 +0200

    Created Room Feature mapping table

---
+++ HotBooking.Data/Models/RoomFeature.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotBooking.Data.Models;
+public class RoomFeature
+{
+    [Required]
+    public int FeatureId { get; set; }
+
+    [Required, ForeignKey(nameof(FeatureId))]
+    public Feature Feature { get; set; } = null!;
+
+    [Required]
+    public int RoomId { get; set; }
+
+    [Required, ForeignKey(nameof(RoomId))]
+    public Room Room { get; set; } = null!;
+}
