abe6d63cd32fcefceb7e8b4e7644cc3c2189546f
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-03 13:26:55 +0200

    Extended Identity User

---
+++ HotBooking.Data/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HotBooking.Data.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public ICollection<Booking> Bookings { get; set; } = new HashSet<Booking>();
+
+    public ICollection<Review> Reviews { get; set; } = new HashSet<Review>();
+}
