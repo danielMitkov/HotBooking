615d6f2cfa76645fbedca4422b7dfc67a42ecbe2
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-03 13:26:14 +0200

    Created Hotel Facility mapping table

---
+++ HotBooking.Data/Models/HotelFacility.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotBooking.Data.Models;
+public class HotelFacility
+{
+    [Required]
+    public int FacilityId { get; set; }
+
+    [Required, ForeignKey(nameof(FacilityId))]
+    public Facility Facility { get; set; } = null!;
+
+    [Required]
+    public int HotelId { get; set; }
+
+    [Required, ForeignKey(nameof(HotelId))]
+    public Hotel Hotel { get; set; } = null!;
+}
