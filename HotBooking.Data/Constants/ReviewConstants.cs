cdfcf63a29a03db20a6edf49b920763e44846007
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-03 15:25:20 +0200

    Create ReviewConstants.cs

---
+++ HotBooking.Data/Constants/ReviewConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace HotBooking.Data.Constants;
+public static class ReviewConstants
+{
+    public const int RatingScoreMin = 1;
+    public const int RatingScoreMax = 10;
+
+    public const int TitleLengthMin = 5;
+    public const int TitleLengthMax = 50;
+
+    public const int CommentMin = 5;
+    public const int CommentMax = 500;
+}
