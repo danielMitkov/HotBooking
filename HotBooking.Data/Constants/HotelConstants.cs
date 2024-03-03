c69151188c6bf5e070ec0dee82272f248967f253
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-03 15:18:46 +0200

    Create HotelConstants.cs

---
+++ HotBooking.Data/Constants/HotelConstants.cs
@@ -0,0 +1,22 @@
+﻿namespace HotBooking.Data.Constants;
+
+public static class HotelConstants
+{
+    public const int HotelNameLengthMin = 3;
+    public const int HotelNameLengthMax = 50;
+
+    public const int DescriptionLengthMin = 10;
+    public const int DescriptionLengthMax = 1000;
+
+    public const int StreetAddressLengthMin = 10;
+    public const int StreetAddressLengthMax = 100;
+
+    public const int CityNameLengthMin = 3;
+    public const int CityNameLengthMax = 50;
+
+    public const int CountryNameLengthMin = 3;
+    public const int CountryNameLengthMax = 50;
+
+    public const int StarRatingMin = 1;
+    public const int StarRatingMax = 5;
+}
