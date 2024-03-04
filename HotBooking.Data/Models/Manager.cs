d58ac1da84f3b6864e9dc64abb897f3325036423
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-04 11:32:25 +0200

    Swapped to ApplicationUser

--- HotBooking.Data/Models/Manager.cs
+++ HotBooking.Data/Models/Manager.cs
@@ -1,17 +1,20 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HotBooking.Data.Models;
 public class Manager
 {
+    [Key]
     public int Id { get; set; }
+
+    [Required]
+    public string PhoneNumber { get; set; } = null!;
 
     [Required]
     public string UserId { get; set; } = null!;
 
     [ForeignKey(nameof(UserId))]
-    public IdentityUser User { get; set; } = null!;
+    public ApplicationUser User { get; set; } = null!;
 
     [Required]
     public int HotelId { get; set; }
