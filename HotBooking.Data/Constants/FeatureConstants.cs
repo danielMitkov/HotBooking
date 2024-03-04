ecca2f1a19d11267a416a2f582da80a8afa62441
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-04 16:40:59 +0200

    Create FeatureConstants.cs

---
+++ HotBooking.Data/Constants/FeatureConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace HotBooking.Data.Constants;
+public static class FeatureConstants
+{
+    public const int NameLengthMin = 2;
+    public const int NameLengthMax = 50;
+
+    public const int IconClassNameLengthMin = 2;
+    public const int IconClassNameLengthMax = 100;
+}
