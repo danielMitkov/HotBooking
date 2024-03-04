52d5dbd444c4ae0debabbed66d5db467c90137ed
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-04 11:29:08 +0200

    DbContext added tables

--- HotBooking.Data/HotBookingDbContext.cs
+++ HotBooking.Data/HotBookingDbContext.cs
@@ -1,11 +1,32 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using HotBooking.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotBooking.Data;
-public class HotBookingDbContext:IdentityDbContext
+public class HotBookingDbContext : IdentityDbContext<ApplicationUser>
 {
     public HotBookingDbContext(DbContextOptions<HotBookingDbContext> options)
         : base(options)
     {
     }
+
+    public DbSet<Hotel> Hotels { get; set; } = null!;
+
+    public DbSet<Room> Rooms { get; set; } = null!;
+
+    public DbSet<Booking> Bookings { get; set; } = null!;
+
+    public DbSet<Review> Reviews { get; set; } = null!;
+
+    public DbSet<Manager> Managers { get; set; } = null!;
+
+    public DbSet<Feature> Features { get; set; } = null!;
+
+    public DbSet<Facility> Facilities { get; set; } = null!;
+
+    public DbSet<ImageUrl> ImageUrls { get; set; } = null!;
+
+    public DbSet<RoomFeature> RoomsFeatures { get; set; } = null!;
+
+    public DbSet<HotelFacility> HotelsFacilities { get; set; } = null!;
 }
