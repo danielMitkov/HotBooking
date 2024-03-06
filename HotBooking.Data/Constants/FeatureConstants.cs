d610d80d37e716e97bae94bd758142c7d402d58b
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-06 22:21:46 +0200

    Increased max length

--- HotBooking.Data/Constants/FeatureConstants.cs
+++ HotBooking.Data/Constants/FeatureConstants.cs
@@ -4,6 +4,6 @@ public static class FeatureConstants
     public const int NameLengthMin = 2;
     public const int NameLengthMax = 50;
 
-    public const int IconClassNameLengthMin = 2;
-    public const int IconClassNameLengthMax = 100;
+    public const int SvgTagLengthMin = 2;
+    public const int SvgTagLengthMax = 3000;
 }
