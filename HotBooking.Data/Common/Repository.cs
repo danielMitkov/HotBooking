f1e9a925f3220446e256650d7676927a8eb1c706
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-07 15:17:54 +0200

    Created Unit of Work

---
+++ HotBooking.Data/Common/Repository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotBooking.Data.Common;
+public class Repository : IRepository
+{
+    private readonly HotBookingDbContext dbContext;
+
+    public Repository(HotBookingDbContext context)
+    {
+        dbContext = context;
+    }
+
+    public IQueryable<T> All<T>() where T : class
+    {
+        return dbContext
+            .Set<T>();
+    }
+
+    public IQueryable<T> AllReadOnly<T>() where T : class
+    {
+        return dbContext
+            .Set<T>()
+            .AsNoTracking();
+    }
+
+    public async Task AddAsync<T>(T entity) where T : class
+    {
+        await dbContext
+            .Set<T>()
+            .AddAsync(entity);
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await dbContext.SaveChangesAsync();
+    }
+}
