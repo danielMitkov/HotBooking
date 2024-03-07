ab571341faeb9d4b9f457c6ba9cb83f7378edab6
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-07 15:17:43 +0200

    Created Interface for Unit of Work

---
+++ HotBooking.Data/Common/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace HotBooking.Data.Common;
+public interface IRepository
+{
+    IQueryable<T> All<T>() where T : class;
+
+    IQueryable<T> AllReadOnly<T>() where T : class;
+
+    Task AddAsync<T>(T entity) where T : class;
+
+    Task SaveChangesAsync();
+}
