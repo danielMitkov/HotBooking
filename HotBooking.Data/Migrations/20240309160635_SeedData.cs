9fb0ad8a9b94abae6f4d9483cd085a4bad5f0f14
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-09 22:15:08 +0200

    Create 20240309160635_SeedData.cs

---
+++ HotBooking.Data/Migrations/20240309160635_SeedData.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotBooking.Data.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "409a1fe1-a017-4e11-94fb-7c638937c52d", 0, "c23831a4-fd9a-4168-a64e-c0fe4a434456", "manager@mail.com", false, false, null, "manager@mail.com", "manager@mail.com", "AQAAAAEAACcQAAAAEA7WdWhx2YuxmX6p6htYKiVAqo92ek1hdrq2dEAVp6ttzUXVUVls8EwyYX3zkjZ7Hg==", null, false, "4c3b5885-b95a-4b0b-99f8-fd2afff32c80", false, "manager@mail.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "49c62027-4afc-4a6f-89b4-58b946cc51a8", 0, "256fd113-02aa-4030-bc84-4af8a82553b8", "guest@mail.com", false, false, null, "guest@mail.com", "guest@mail.com", "AQAAAAEAACcQAAAAEOqS6fpT4gfUiwVA4PRCrhqiehyyFYT5JTyjHr9wAh4NGFXByABQWvF4V6mchNh18A==", null, false, "d42d8e08-974a-4961-9cfc-d42904c76d2c", false, "guest@mail.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "a9dbd6b4-a878-41a6-9da6-8fac71893547", 0, "d6b1e6a6-94f4-46d4-8f69-dd211c7f9d0e", "two@mail.com", false, false, null, "two@mail.com", "two@mail.com", "AQAAAAEAACcQAAAAEEc6DrLM8cnvKh1TsFwxM14VS85W0B4V9r4qzbdbEhsLBav0Fn5Neb/xj/H0HAhdRg==", null, false, "d3577acc-108b-4bc7-8e83-a975c1179a4d", false, "two@mail.com" });
+
+            migrationBuilder.InsertData(
+                table: "Managers",
+                columns: new[] { "Id", "PhoneNumber", "UserId" },
+                values: new object[] { 1, "08812345678", "409a1fe1-a017-4e11-94fb-7c638937c52d" });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "CityName", "CountryName", "Description", "HotelName", "ManagerId", "StarRating", "StreetAddress" },
+                values: new object[] { 1, "London", "United Kingdom", "Less than a 5-minute walk from London Paddington Station and Hyde Park, this boutique hotel offers elegant rooms with free internet and satellite TV.", "The Chilworth London Paddington", 1, 5, "Westminster Borough" });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "CityName", "CountryName", "Description", "HotelName", "ManagerId", "StarRating", "StreetAddress" },
+                values: new object[] { 2, "Bansko", "Bulgaria", "Get your trip off to a great start with a stay at this property, which offers free Wi-Fi in all rooms. Conveniently situated in the Bansko part of Bansko, this property puts you close to attractions and interesting dining options. Rated with 5 stars, this high-quality property provides guests with access to massage, restaurant and hot tub on-site.", "Kempinski Hotel Grand Arena Bansko", 1, 4, "#96 Pirin Street" });
+
+            migrationBuilder.InsertData(
+                table: "HotelImageUrls",
+                columns: new[] { "Id", "HotelId", "Url" },
+                values: new object[,]
+                {
+                    { 1, 1, "https://www.w3schools.com/html/pic_trulli.jpg" },
+                    { 2, 2, "https://pix8.agoda.net/hotelImages/182146/-1/112f1fa0f38baf10800569462deb46cd.jpg" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "Id", "BedsCount", "Description", "HotelId", "PricePerNight", "RoomSizeSquareMeters", "Title" },
+                values: new object[,]
+                {
+                    { 1, 2, "A comfortable room for a relaxing stay.", 1, 75.50m, 25, "Cozy Retreat" },
+                    { 2, 1, "Luxurious suite with modern amenities.", 1, 120.75m, 40, "Executive Suite" },
+                    { 3, 3, "Spacious room suitable for families.", 2, 95.25m, 35, "Family Getaway" },
+                    { 4, 1, "Enjoy breathtaking views of the ocean.", 2, 110.00m, 30, "Ocean View Paradise" },
+                    { 5, 2, "Escape to a cozy lodge in the mountains.", 2, 85.80m, 28, "Mountain Lodge" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Bookings",
+                columns: new[] { "Id", "AdultsCount", "CheckIn", "CheckOut", "HotelId", "RoomId", "UserId" },
+                values: new object[,]
+                {
+                    { 1, 2, new DateTime(2023, 5, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 5, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, 1, "49c62027-4afc-4a6f-89b4-58b946cc51a8" },
+                    { 2, 3, new DateTime(2023, 6, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 6, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, 2, "49c62027-4afc-4a6f-89b4-58b946cc51a8" },
+                    { 3, 1, new DateTime(2023, 7, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 7, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, 3, "a9dbd6b4-a878-41a6-9da6-8fac71893547" },
+                    { 4, 4, new DateTime(2023, 8, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 8, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, 4, "a9dbd6b4-a878-41a6-9da6-8fac71893547" },
+                    { 5, 2, new DateTime(2023, 9, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 9, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, 5, "a9dbd6b4-a878-41a6-9da6-8fac71893547" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Reviews",
+                columns: new[] { "Id", "BookingId", "Comment", "HotelId", "RatingScore", "ReviewedOn", "Title", "UserId" },
+                values: new object[,]
+                {
+                    { 1, 1, "I love this product! It's exactly what I needed.", 1, 5, new DateTime(2023, 4, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Excellent Product", "49c62027-4afc-4a6f-89b4-58b946cc51a8" },
+                    { 2, 2, "The product was okay, but could be better.", 1, 3, new DateTime(2023, 3, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), "Average Experience", "49c62027-4afc-4a6f-89b4-58b946cc51a8" },
+                    { 3, 3, "I was very disappointed with this product.", 2, 1, new DateTime(2023, 3, 27, 0, 0, 0, 0, DateTimeKind.Unspecified), "Poor Quality", "a9dbd6b4-a878-41a6-9da6-8fac71893547" },
+                    { 4, 4, "I got my money's worth with this product.", 2, 4, new DateTime(2023, 3, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), "Good Value", "a9dbd6b4-a878-41a6-9da6-8fac71893547" },
+                    { 5, 5, "This product is amazing! Highly recommended.", 2, 5, new DateTime(2023, 3, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), "Best Product Ever", "a9dbd6b4-a878-41a6-9da6-8fac71893547" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "HotelImageUrls",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "HotelImageUrls",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "49c62027-4afc-4a6f-89b4-58b946cc51a8");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a9dbd6b4-a878-41a6-9da6-8fac71893547");
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Managers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "409a1fe1-a017-4e11-94fb-7c638937c52d");
+        }
+    }
+}
