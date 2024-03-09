75b2fb47e39107313287139eb65756146baa6dd6
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-09 22:14:37 +0200

    Create ManagerConfiguration.cs

---
+++ HotBooking.Data/Configurations/ManagerConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using HotBooking.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HotBooking.Data.Configurations;
+public class ManagerConfiguration : IEntityTypeConfiguration<Manager>
+{
+    public void Configure(EntityTypeBuilder<Manager> builder)
+    {
+        builder.HasData(GetManagers());
+    }
+
+    private Manager[] GetManagers()
+    {
+        return new Manager[]
+        {
+            new Manager()
+            {
+                Id = 1,
+                PhoneNumber = "08812345678",
+                UserId = "409a1fe1-a017-4e11-94fb-7c638937c52d"
+            }
+        };
+    }
+}
