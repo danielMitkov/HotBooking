5f577631410fb3b19a7714368fe1f5c2fd13435a
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-09 22:18:22 +0200

    Added list action with paging

--- HotBooking.Web/Controllers/HotelsController.cs
+++ HotBooking.Web/Controllers/HotelsController.cs
@@ -12,4 +12,29 @@ public class HotelsController : Controller
     {
         this.hotelsService = hotelsService;
     }
+
+    public async Task<IActionResult> List(int page = 1)
+    {
+        var hotels = await hotelsService.AllHotelsAsync();
+
+        const int pageSize = 1;
+
+        if (page < 1)
+        {
+            page = 1;
+        }
+
+        Pager pager = new Pager(hotels.Count(), page, pageSize);
+
+        int itemsToSkip = (page - 1) * pageSize;
+
+        var pageItems = hotels
+            .Skip(itemsToSkip)
+            .Take(pager.PageSize)
+            .ToArray();
+
+        ViewBag.Pager = pager;
+
+        return View(pageItems);
+    }
 }
