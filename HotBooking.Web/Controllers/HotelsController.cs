d0246aa4b337e159ee497a7ecfb2dae725ed26cb
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-09 22:17:58 +0200

    Create HotelsController.cs

---
+++ HotBooking.Web/Controllers/HotelsController.cs
@@ -0,0 +1,15 @@
+﻿using HotBooking.Core.Interfaces;
+using HotBooking.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotBooking.Web.Controllers;
+
+public class HotelsController : Controller
+{
+    private readonly IHotelsService hotelsService;
+
+    public HotelsController(IHotelsService hotelsService)
+    {
+        this.hotelsService = hotelsService;
+    }
+}
