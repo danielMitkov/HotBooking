3af8cd5cfa414e4960434e32df63d5a629ce6af9
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-09 22:18:29 +0200

    Create Pager.cs

---
+++ HotBooking.Web/Models/Pager.cs
@@ -0,0 +1,44 @@
+﻿namespace HotBooking.Web.Models;
+
+public class Pager
+{
+    public Pager()
+    {
+
+    }
+
+    public Pager(int totalItems, int page, int pageSize)
+    {
+        int totalPages = (int)Math.Ceiling(totalItems / (decimal)pageSize);
+        int currentPage = page;
+
+        int startPage = currentPage - 1;
+        int endPage = currentPage + 1;
+
+        if (startPage <= 0)
+        {
+            startPage = 1;
+        }
+
+        if (endPage > totalPages)
+        {
+            endPage = totalPages;
+        }
+
+        TotalItems = totalItems;
+        CurrentPage = currentPage;
+        PageSize = pageSize;
+
+        TotalPages = totalPages;
+        StartPage = startPage;
+        EndPage = endPage;
+    }
+
+    public int TotalItems { get; private set; }
+    public int CurrentPage { get; private set; }
+    public int PageSize { get; private set; }
+
+    public int TotalPages { get; private set; }
+    public int StartPage { get; private set; }
+    public int EndPage { get; private set; }
+}
