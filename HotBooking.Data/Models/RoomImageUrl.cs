a729c885f4c004f1f9f47e8d00541085b52ef2db
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-09 22:07:30 +0200

    Create RoomImageUrl.cs

---
+++ HotBooking.Data/Models/RoomImageUrl.cs
@@ -0,0 +1,20 @@
+﻿using HotBooking.Data.Constants;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotBooking.Data.Models;
+public class RoomImageUrl
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(RoomImageUrlConstants.UrlLengthMax)]
+    public string Url { get; set; } = null!;
+
+    [Required]
+    public int RoomId { get; set; }
+
+    [ForeignKey(nameof(RoomId))]
+    public Room Room { get; set; } = null!;
+}
