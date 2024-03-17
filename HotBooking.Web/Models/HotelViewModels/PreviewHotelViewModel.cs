b4efcf90bdc2bfef4bac2881df8675f5dfd33b14
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-17 23:13:29 +0200

    Create PreviewHotelViewModel.cs

---
+++ HotBooking.Web/Models/HotelViewModels/PreviewHotelViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace HotBooking.Web.Models.HotelViewModels;
+
+public class PreviewHotelViewModel
+{
+    public int Id { get; set; }
+
+    public string ImageUrl { get; set; } = null!;
+
+    public string HotelName { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string FullAddress { get; set; } = null!;
+
+    public int StarRating { get; set; }
+
+    public decimal AverageRating { get; set; }
+
+    public int ReviewsCount { get; set; }
+}
