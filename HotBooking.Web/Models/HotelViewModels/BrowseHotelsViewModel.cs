988f1c97d5a20fca74716345739572bf3c3fbba1
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-17 11:33:44 +0200

    Create BrowseHotelsViewModel.cs

---
+++ HotBooking.Web/Models/HotelViewModels/BrowseHotelsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using HotBooking.Core.DTOs.HotelDtos;
+
+namespace HotBooking.Web.Models.HotelViewModels;
+
+public class BrowseHotelsViewModel
+{
+    public string? City { get; set; }
+
+    public int Page { get; set; } = 1;
+
+    public string CitiesJson { get; set; } = null!;
+
+    public ICollection<PreviewHotelDto> Hotels { get; set; } = null!;
+
+    public Pager<PreviewHotelDto> Pager { get; set; } = null!;
+}
