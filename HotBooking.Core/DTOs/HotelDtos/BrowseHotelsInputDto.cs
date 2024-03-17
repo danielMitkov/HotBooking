f7e95558a6c55a1378153d9919e70a7b83509a0e
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-17 11:34:01 +0200

    Create BrowseHotelsInputDto.cs

---
+++ HotBooking.Core/DTOs/HotelDtos/BrowseHotelsInputDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HotBooking.Core.DTOs.HotelDtos;
+public class BrowseHotelsInputDto
+{
+    public int CurrentPage { get; set; }
+
+    public int PageSize { get; set; }
+
+    public string? City { get; set; }
+}
