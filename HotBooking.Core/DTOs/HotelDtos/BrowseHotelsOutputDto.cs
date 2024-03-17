c6cb371d71971e51d20229ae11c12f2c156d3a4a
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-17 11:33:57 +0200

    Create BrowseHotelsOutputDto.cs

---
+++ HotBooking.Core/DTOs/HotelDtos/BrowseHotelsOutputDto.cs
@@ -0,0 +1,7 @@
+﻿namespace HotBooking.Core.DTOs.HotelDtos;
+public class BrowseHotelsOutputDto
+{
+    public ICollection<PreviewHotelDto> SelectedHotels { get; set; } = null!;
+
+    public int TotalPages { get; set; }
+}
