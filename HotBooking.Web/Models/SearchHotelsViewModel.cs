d091b29018e40061e2013101fba2619885f3730d
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-25 07:37:46 +0200

    Create SearchHotelsViewModel.cs

---
+++ HotBooking.Web/Models/SearchHotelsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using HotBooking.Data.Constants;
+using HotBooking.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotBooking.Web.Models;
+
+public class SearchHotelsViewModel
+{
+    [Required]
+    [MinLength(HotelConstants.CityNameLengthMin)]
+    [MaxLength(HotelConstants.CityNameLengthMax)]
+    //[Remote(nameof(ValidationController.CityExists), ValidationController.Name)]
+    public string City { get; set; } = null!;
+
+    [Required]
+    //[Remote(nameof(ValidationController.AreDatesValid), ValidationController.Name, AdditionalFields = nameof(CheckOutDate))]
+    public DateTime CheckInDate { get; set; }
+
+    [Required]
+    //[Remote(nameof(ValidationController.AreDatesValid), ValidationController.Name, AdditionalFields = nameof(CheckInDate))]
+    public DateTime CheckOutDate { get; set; }
+
+    [Required]
+    [Range(BookingConstants.AdultsCountMin, BookingConstants.AdultsCountMax)]
+    public int AdultsCount { get; set; }
+}
