2deab1946ac3b0011969555848aa91e04b350e10
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-25 07:37:33 +0200

    Create BaseController.cs

---
+++ HotBooking.Web/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotBooking.Web.Controllers;
+
+[Authorize]
+public class BaseController : Controller
+{
+    protected IActionResult InternalServerError(string message)
+    {
+        return RedirectToAction("Error", "Home", new { statusCode = 500, message = message });
+    }
+
+    protected IActionResult RedirectToError<T>(ILogger<T> logger, Exception exception, int statusCode)
+    {
+        logger.LogError(exception, statusCode.ToString());
+
+        return RedirectToAction("Error", "Home", new { statusCode = statusCode, message = exception.Message });
+    }
+}
