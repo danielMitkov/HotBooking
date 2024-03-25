1435a697ec1b7126f8fb37f79f232f361d91a1d9
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-25 07:37:39 +0200

    Create ClaimsPrincipalExtensions.cs

---
+++ HotBooking.Web/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace HotBooking.Web.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static string GetUserId(this ClaimsPrincipal user)
+    {
+        return user.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
