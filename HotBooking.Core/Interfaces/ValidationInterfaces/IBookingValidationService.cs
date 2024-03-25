13ecad1103b46cefef9694543cb1d4e2a778d758
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-25 07:37:20 +0200

    Create IBookingValidationService.cs

---
+++ HotBooking.Core/Interfaces/ValidationInterfaces/IBookingValidationService.cs
@@ -0,0 +1,7 @@
+﻿namespace HotBooking.Core.Interfaces.ValidationInterfaces;
+public interface IBookingValidationService
+{
+    bool IsDateNotInThePast(DateTime date);
+    bool IsCheckOutAfterCheckIn(DateTime checkInDate, DateTime checkOutDate);
+    string? AreDatesValid(DateTime checkInDate, DateTime checkOutDate);
+}
