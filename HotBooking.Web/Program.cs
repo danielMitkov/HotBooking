6fa1bfa69087d44d3e12bbe2562177fe7de66997
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-25 07:41:46 +0200

    added status code pages

--- HotBooking.Web/Program.cs
+++ HotBooking.Web/Program.cs
@@ -1,3 +1,5 @@
+using HotBooking.Web.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationDbContext(builder.Configuration);
@@ -11,15 +13,20 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseMigrationsEndPoint();
 }
 else
 {
     app.UseExceptionHandler("/Home/Error");
+    app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
+
     app.UseHsts();
 }
 
+
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
