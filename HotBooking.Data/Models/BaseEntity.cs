4b9d72177b48cc092e65fc224df3583f75e78f29
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-25 07:37:08 +0200

    Create BaseEntity.cs

---
+++ HotBooking.Data/Models/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotBooking.Data.Models;
+public abstract class BaseEntity
+{
+    protected BaseEntity()
+    {
+        PublicId = Guid.NewGuid();
+        IsActive = true;
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public Guid PublicId { get; set; }
+
+    [Required]
+    public bool IsActive { get; set; }
+}
