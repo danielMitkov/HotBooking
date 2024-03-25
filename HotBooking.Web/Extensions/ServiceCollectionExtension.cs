bebe560067498cbf853cea97af803bc3a05239bd
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-25 07:43:18 +0200

    Update ServiceCollectionExtension.cs

--- HotBooking.Web/Extensions/ServiceCollectionExtension.cs
+++ HotBooking.Web/Extensions/ServiceCollectionExtension.cs
@@ -1,17 +1,22 @@
 ﻿using HotBooking.Core.Interfaces;
+using HotBooking.Core.Interfaces.ValidationInterfaces;
 using HotBooking.Core.Services;
+using HotBooking.Core.Services.ValidationServices;
 using HotBooking.Data;
 using HotBooking.Data.Common;
 using HotBooking.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace Microsoft.Extensions.DependencyInjection;
+namespace HotBooking.Web.Extensions;
 
 public static class ServiceCollectionExtension
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddScoped<IHotelsService, HotelsService>();
+        services.AddScoped<IHotelValidationService, HotelValidationService>();
+
+        services.AddScoped<IBookingValidationService, BookingValidationService>();
 
         return services;
     }
@@ -34,7 +39,10 @@ public static class ServiceCollectionExtension
     public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
     {
         services
-            .AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddDefaultIdentity<ApplicationUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+            })
             .AddEntityFrameworkStores<HotBookingDbContext>();
 
         return services;
