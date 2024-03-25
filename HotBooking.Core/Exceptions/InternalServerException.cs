5ac766546d352cdc271e46bb12ad3511c6ca692b
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-25 07:37:22 +0200

    Create InternalServerException.cs

---
+++ HotBooking.Core/Exceptions/InternalServerException.cs
@@ -0,0 +1,9 @@
+﻿namespace HotBooking.Core.Exceptions;
+
+public class InternalServerException : Exception
+{
+    public InternalServerException(string restOfTheMessage)
+        : base("Unexpected error occured while trying to " + restOfTheMessage)
+    {
+    }
+}
