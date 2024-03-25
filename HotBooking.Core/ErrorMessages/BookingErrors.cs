a62fbc4de163c4580a9fb8cfbe185c3c0102bf5c
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-25 07:37:28 +0200

    Create BookingErrors.cs

---
+++ HotBooking.Core/ErrorMessages/BookingErrors.cs
@@ -0,0 +1,8 @@
+﻿namespace HotBooking.Core.ErrorMessages;
+public static class BookingErrors
+{
+    public const string CheckInDateInThePast = "Check-in date cannot be in the past.";
+    public const string CheckOutDateInThePast = "Check-out date cannot be in the past.";
+
+    public const string CheckInDateAfterCheckOutDate = "Check-out date must be after check-in date.";
+}
