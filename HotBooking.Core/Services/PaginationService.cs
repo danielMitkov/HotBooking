308583e5e20897a52fff4dbfb87a1d0cb09c22d8
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-27 09:51:46 +0200

    Create PaginationService.cs

---
+++ HotBooking.Core/Services/PaginationService.cs
@@ -0,0 +1,32 @@
+﻿using HotBooking.Core.Exceptions;
+using HotBooking.Core.Interfaces;
+using HotBooking.Data.Models;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace HotBooking.Core.Services;
+
+public class PaginationService: IPaginationService
+{
+    public int GetTotalPages(int allHotelsCount, int pageSize, int currentPage)
+    {
+        int totalPages = (int)Math.Ceiling(allHotelsCount / (decimal)pageSize);
+
+        if (currentPage < 1 || currentPage > totalPages)
+        {
+            throw new PageOutOfRangeException(totalPages);
+        }
+
+        return totalPages;
+    }
+
+    public IQueryable<Hotel> ApplyPagination(IQueryable<Hotel> query, int pageSize, int currentPage)
+    {
+        int skipAmount = (currentPage - 1) * pageSize;
+
+        query = query
+            .Skip(skipAmount)
+            .Take(pageSize);
+
+        return query;
+    }
+}
