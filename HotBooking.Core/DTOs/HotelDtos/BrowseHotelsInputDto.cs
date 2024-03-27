0172e51998142a525726c0af14e0a8e2059341ac
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-27 09:53:48 +0200

    changed to record

--- HotBooking.Core/DTOs/HotelDtos/BrowseHotelsInputDto.cs
+++ HotBooking.Core/DTOs/HotelDtos/BrowseHotelsInputDto.cs
@@ -1,13 +1,12 @@
 ﻿using HotBooking.Core.Enums;
 
 namespace HotBooking.Core.DTOs.HotelDtos;
-public class BrowseHotelsInputDto
-{
-    public int CurrentPage { get; set; }
 
-    public int PageSize { get; set; }
-
-    public string City { get; set; } = null!;
-
-    public HotelSorting Sorting { get; set; }
-}
+public record BrowseHotelsInputDto
+(
+    int CurrentPage,
+    int PageSize,
+    string City,
+    HotelSorting Sorting,
+    ICollection<Guid> FacilitySelectedPublicIds
+);
