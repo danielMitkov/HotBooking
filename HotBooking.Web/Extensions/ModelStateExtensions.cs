e064b73e26153ff2995c174d7b7b2c9eb7ab4d27
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-27 10:06:59 +0200

    removed leaky validation

--- HotBooking.Web/Extensions/ModelStateExtensions.cs
+++ HotBooking.Web/Extensions/ModelStateExtensions.cs
@@ -1,38 +1,9 @@
-﻿using HotBooking.Core.ErrorMessages;
-using HotBooking.Core.Interfaces.ValidationInterfaces;
-using HotBooking.Web.Models;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace HotBooking.Web.Extensions;
 
 public static class ModelStateExtensions
 {
-    public static async Task AddModelErrorsForSearchHotelsViewModel(this ModelStateDictionary modelState, 
-        SearchHotelsViewModel viewModel, 
-        IHotelValidationService hotelValidationService, 
-        IBookingValidationService bookingValidationService)
-    {
-        if ((await hotelValidationService.IsCityFoundAsync(viewModel.City)) == false)
-        {
-            modelState.AddModelError(nameof(viewModel.City), HotelErrors.CityNotFound);
-        }
-
-        if (bookingValidationService.IsDateNotInThePast(viewModel.CheckInDate) == false)
-        {
-            modelState.AddModelError(nameof(viewModel.CheckInDate), BookingErrors.CheckInDateInThePast);
-        }
-
-        if (bookingValidationService.IsDateNotInThePast(viewModel.CheckOutDate) == false)
-        {
-            modelState.AddModelError(nameof(viewModel.CheckOutDate), BookingErrors.CheckOutDateInThePast);
-        }
-
-        if (bookingValidationService.IsCheckOutAfterCheckIn(viewModel.CheckInDate, viewModel.CheckOutDate) == false)
-        {
-            modelState.AddModelError(nameof(viewModel.CheckOutDate), BookingErrors.CheckInDateAfterCheckOutDate);
-        }
-    }
-
     public static bool IsValidAfterAddingErrors(this ModelStateDictionary modelState, IDictionary<string, string> modelErrors)
     {
         foreach ((string key, string errorMessage) in modelErrors)
