f418f4971a7668872131d4323915b9a92b2226c3
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-27 09:53:55 +0200

    changed to record

--- HotBooking.Core/DTOs/HotelDtos/PreviewHotelDto.cs
+++ HotBooking.Core/DTOs/HotelDtos/PreviewHotelDto.cs
@@ -1,21 +1,13 @@
 ﻿namespace HotBooking.Core.DTOs.HotelDtos;
-public class PreviewHotelDto
-{
-    public int Id { get; set; }
 
-    public string ImageUrl { get; set; } = null!;
-
-    public string HotelName { get; set; } = null!;
-
-    public string Description { get; set; } = null!;
-
-    public string StreetAddress { get; set; } = null!;
-
-    public string CityName { get; set; } = null!;
-
-    public int StarRating { get; set; }
-
-    public decimal AverageRating  { get; set; }
-
-    public int ReviewsCount { get; set; }
-}
+public record PreviewHotelDto(
+    int Id,
+    string ImageUrl,
+    string HotelName,
+    string Description,
+    string StreetAddress,
+    string CityName,
+    int StarRating,
+    decimal AverageRating,
+    int ReviewsCount
+);
