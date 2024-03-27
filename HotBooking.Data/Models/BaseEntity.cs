5bd894cffa7a8f17d5d535513fecdafa32ef1ac3
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-27 09:57:23 +0200

    removed constructor

--- HotBooking.Data/Models/BaseEntity.cs
+++ HotBooking.Data/Models/BaseEntity.cs
@@ -1,20 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
 namespace HotBooking.Data.Models;
+
 public abstract class BaseEntity
 {
-    protected BaseEntity()
-    {
-        PublicId = Guid.NewGuid();
-        IsActive = true;
-    }
-
     [Key]
     public int Id { get; set; }
 
     [Required]
-    public Guid PublicId { get; set; }
+    public Guid PublicId { get; set; } = Guid.NewGuid();
 
     [Required]
-    public bool IsActive { get; set; }
+    public bool IsActive { get; set; } = true;
 }
