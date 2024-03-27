20710efcc70c131d352dbc5eeb738b85332ce674
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-27 09:55:54 +0200

    added materialization methods

--- HotBooking.Data/Common/IRepository.cs
+++ HotBooking.Data/Common/IRepository.cs
@@ -1,15 +1,16 @@
-﻿namespace HotBooking.Data.Common;
+﻿using HotBooking.Data.Models;
+
+namespace HotBooking.Data.Common;
 public interface IRepository
 {
-    IQueryable<T> All<T>() where T : class;
-
-    IQueryable<T> AllReadOnly<T>() where T : class;
-
+    IQueryable<T> All<T>() where T : BaseEntity;
+    IQueryable<T> AllReadOnly<T>() where T : BaseEntity;
     Task AddAsync<T>(T entity) where T : class;
-
     Task SaveChangesAsync();
 
     Task<T?> GetByIdAsync<T>(object id) where T : class;
 
     Task<bool> DeleteAsync<T>(object id) where T : class;
+    Task<ICollection<T>> ToICollectionAsync<T>(IQueryable<T> query) where T : class;
+    Task<int> CountAsync<T>(IQueryable<T> query) where T : class;
 }
