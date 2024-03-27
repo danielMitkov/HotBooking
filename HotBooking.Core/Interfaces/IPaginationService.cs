961e50727378cef2bcc2d52f787390acbfea71cb
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-27 09:52:05 +0200

    Create IPaginationService.cs

---
+++ HotBooking.Core/Interfaces/IPaginationService.cs
@@ -0,0 +1,9 @@
+﻿using HotBooking.Data.Models;
+
+namespace HotBooking.Core.Interfaces;
+
+public interface IPaginationService
+{
+    int GetTotalPages(int allHotelsCount, int pageSize, int currentPage);
+    IQueryable<Hotel> ApplyPagination(IQueryable<Hotel> query, int pageSize, int currentPage);
+}
