a160e1cb23f654e8d2fb8f942ca881010d1199e1
danielMitkov <daniel.mitkov01@gmail.com>
2024-03-30 09:15:53 +0200

    extracted auto complete into js file

---
+++ HotBooking.Web/wwwroot/js/autocomplete.js
@@ -0,0 +1,19 @@
+﻿$("#searchBar").autocomplete({
+    source: function (request, response) {
+        let controllerName = $('#urlInfo').attr('data-controllerName');
+        let actionName = $('#urlInfo').attr('data-actionName');
+
+        let url = `/${controllerName}/${actionName}/`;
+
+        let searchTerm = $('#searchBar').val();
+
+        $.ajax({
+            url: url,
+            data: { searchTerm: searchTerm },
+            success: function (cities) {
+                response(cities);
+            }
+        });
+    },
+    minLength: 2
+});
