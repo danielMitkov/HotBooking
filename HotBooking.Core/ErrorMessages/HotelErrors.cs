203d24e93b061494b2e8b2a9232edf2e82bf4135
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-04 11:36:43 +0300

    added PageNumberOutOfRange

--- HotBooking.Core/ErrorMessages/HotelErrors.cs
+++ HotBooking.Core/ErrorMessages/HotelErrors.cs
@@ -2,4 +2,5 @@
 public static class HotelErrors
 {
     public const string CityNotFound = "City Not Found!";
+    public const string PageNumberOutOfRange = "The Page Number must be between 1 and {0}";
 }
