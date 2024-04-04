a473d1f45afce6c4e9b3fa06c896517ec5fd6fdf
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-04 11:44:35 +0300

    Deleted Pagination service

--- HotBooking.Core/Interfaces/IPaginationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using HotBooking.Data.Models;
-
-namespace HotBooking.Core.Interfaces;
-
-public interface IPaginationService : IErrorMessageProp
-{
-    int? GetTotalPages(int allHotelsCount, int pageSize, int currentPage);
-    IQueryable<Hotel> ApplyPagination(IQueryable<Hotel> query, int pageSize, int currentPage);
-}
