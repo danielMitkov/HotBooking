440124295fa69e9d8b03fd16fefef3226c42b119
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-05 09:12:11 +0300

    Create IValidationService.cs

---
+++ HotBooking.Core/Interfaces/IValidationService.cs
@@ -0,0 +1,9 @@
+﻿namespace HotBooking.Core.Interfaces;
+
+public interface IValidationService
+{
+    bool IsDateNotInThePast(DateTime date);
+    bool IsCheckOutAfterCheckIn(DateTime checkInDate, DateTime checkOutDate);
+    string? AreDatesValid(DateTime checkInDate, DateTime checkOutDate);
+    Task<string?> IsCityFoundAsync(string city);
+}
