127f837b841f7c425a1de1086a989431010fd0ab
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-06 23:37:05 +0300

    Delete IValidationService.cs

--- HotBooking.Core/Interfaces/IValidationService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HotBooking.Core.Interfaces;
-
-public interface IValidationService
-{
-    bool IsDateNotInThePast(DateTime date);
-    bool IsCheckOutAfterCheckIn(DateTime checkInDate, DateTime checkOutDate);
-    string? AreDatesValid(DateTime checkInDate, DateTime checkOutDate);
-    Task<string?> IsCityFoundAsync(string city);
-}
