fc227f25c43527fd4d469e45368422190ebdbe11
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-06 23:41:33 +0300

    removed error message prop

--- HotBooking.Core/Interfaces/IHotelsService.cs
+++ HotBooking.Core/Interfaces/IHotelsService.cs
@@ -2,8 +2,9 @@
 
 namespace HotBooking.Core.Interfaces;
 
-public interface IHotelsService : IErrorMessageProp
+public interface IHotelsService
 {
-    Task<BrowseHotelsOutputDto?> GetFilteredHotelsAsync(BrowseHotelsInputDto inputDto);
+    Task<BrowseHotelsOutputDto> GetFilteredHotelsAsync(BrowseHotelsInputDto inputDto);
     Task<IEnumerable<string>> GetMatchingCitiesAsync(string searchTerm);
+    Task<bool> IsCityFoundAsync(string city);
 }
