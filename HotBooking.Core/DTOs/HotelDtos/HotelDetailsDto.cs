8cdbf43d24f483b9314edb74f48238dbbed01ec6
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-08 22:37:16 +0300

    Create HotelDetailsDto.cs

---
+++ HotBooking.Core/DTOs/HotelDtos/HotelDetailsDto.cs
@@ -0,0 +1,18 @@
+﻿using HotBooking.Core.DTOs.FacilityDtos;
+using HotBooking.Core.DTOs.RoomDtos;
+
+namespace HotBooking.Core.DTOs.HotelDtos;
+
+public record HotelDetailsDto(
+    string HotelName,
+    string Description,
+    string StreetAddress,
+    string CityName,
+    string CountryName,
+    int StarRating,
+    decimal AverageRating,
+    int ReviewsCount,
+    ICollection<FacilityPreviewDto> HotelsFacilities,
+    //ICollection<RoomPreviewDto> Rooms,
+    ICollection<string> ImagesUrls
+);
