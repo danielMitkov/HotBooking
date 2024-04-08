08191c9c487f6b80452814fd7bf5e07462199ba9
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-08 22:38:50 +0300

    removed ControllerName and ActionName

--- HotBooking.Web/Models/PagerViewModel.cs
+++ HotBooking.Web/Models/PagerViewModel.cs
@@ -2,13 +2,10 @@
 
 public class PagerViewModel
 {
-    public PagerViewModel(int totalPages, int currentPage, string controllerName, string actionName)
+    public PagerViewModel(int totalPages, int currentPage)
     {
         TotalPages = totalPages;
         CurrentPage = currentPage;
-
-        ControllerName = controllerName;
-        ActionName = actionName;
 
         int startPage = CurrentPage - 1;
         int endPage = CurrentPage + 1;
@@ -29,10 +26,6 @@ public class PagerViewModel
 
     public int TotalPages { get; set; }
     public int CurrentPage { get; set; }
-
-    public string ControllerName { get; set; }
-    public string ActionName { get; set; }
-
     public int StartPage { get; set; }
     public int EndPage { get; set; }
 }
