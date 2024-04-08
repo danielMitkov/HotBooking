530ae0b5dde10180e0b8485233171bcd80dd9b59
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-08 22:37:21 +0300

    Create HotelDetailsViewModel.cs

---
+++ HotBooking.Web/Models/HotelViewModels/HotelDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using HotBooking.Core.DTOs.HotelDtos;
+
+namespace HotBooking.Web.Models.HotelViewModels;
+
+public class HotelDetailsViewModel
+{
+    public SearchHotelsViewModel Search { get; set; } = null!;
+
+    public HotelDetailsDto Hotel { get; set; } = null!;
+}
