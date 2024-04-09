066928d5aa059c9a8adc5ca0cc6f22bb1c0e7984
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-09 22:54:32 +0300

    renamed to HotelDetailsDtoOutput

--- HotBooking.Web/Models/HotelViewModels/HotelDetailsViewModel.cs
+++ HotBooking.Web/Models/HotelViewModels/HotelDetailsViewModel.cs
@@ -6,5 +6,5 @@ public class HotelDetailsViewModel
 {
     public SearchHotelsViewModel Search { get; set; } = null!;
 
-    public HotelDetailsDto Hotel { get; set; } = null!;
+    public HotelDetailsDtoOutput Hotel { get; set; } = null!;
 }
