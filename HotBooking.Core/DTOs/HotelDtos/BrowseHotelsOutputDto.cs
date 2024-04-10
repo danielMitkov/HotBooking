5d308f193e46274b17ead12429aca922c4e53ee0
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-10 09:00:33 +0300

    renamed dtos

--- HotBooking.Core/DTOs/HotelDtos/BrowseHotelsOutputDto.cs
+++ HotBooking.Core/DTOs/HotelDtos/BrowseHotelsOutputDto.cs
@@ -3,8 +3,8 @@
 namespace HotBooking.Core.DTOs.HotelDtos;
 
 public record BrowseHotelsOutputDto(
-    IEnumerable<PreviewHotelDto> SelectedHotels,
-    IEnumerable<FacilityDto> Facilities,
+    IEnumerable<HotelPreviewDto> SelectedHotels,
+    IEnumerable<FacilityChecksDto> Facilities,
     int TotalPages,
     int AllHotelsCount
 );
