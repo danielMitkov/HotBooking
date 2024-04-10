5cf95da4bb75baf73b2c457a78b6d097d83a5d56
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-10 23:03:15 +0300

    added hotelId

--- HotBooking.Web/Models/HotelViewModels/HotelDetailsViewModel.cs
+++ HotBooking.Web/Models/HotelViewModels/HotelDetailsViewModel.cs
@@ -4,6 +4,8 @@ namespace HotBooking.Web.Models.HotelViewModels;
 
 public class HotelDetailsViewModel
 {
+    public Guid HotelId { get; set; }
+
     public SearchHotelsViewModel Search { get; set; } = null!;
 
     public HotelDetailsDtoOutput Hotel { get; set; } = null!;
