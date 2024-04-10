a4fe689f51ce30743908102a9a917504d603cc88
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-10 23:02:14 +0300

    Create ReviewController.cs

---
+++ HotBooking.Web/Controllers/ReviewController.cs
@@ -0,0 +1,42 @@
+﻿using HotBooking.Core.DTOs.ReviewDtos;
+using HotBooking.Core.Interfaces;
+using HotBooking.Web.Models.ReviewViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotBooking.Web.Controllers;
+
+public class ReviewController : Controller
+{
+    public const string Name = "Review";
+
+    private readonly IReviewService reviewService;
+
+    public ReviewController(IReviewService reviewService)
+    {
+        this.reviewService = reviewService;
+    }
+
+    [Route("Review/All/{page?}")]
+    public async Task<IActionResult> All(Guid hotelId, int page = 1)
+    {
+        BrowseReviewsInputDto inputDto = new(hotelId, page, 2);
+
+        BrowseReviewsOutputDto? outputDto = await reviewService.GetReviewsForHotel(inputDto);
+
+        if (outputDto == null)
+        {
+            TempData["Error"] = "Hotel Not Found!";
+            return BadRequest();
+        }
+
+        BrowseReviewsViewModel model = new()
+        {
+            Page = page,
+            ReviewsCount = outputDto.ReviewsCount,
+            Pager = new(outputDto.TotalPagesCount, page),
+            Reviews = outputDto.Reviews
+        };
+
+        return View(model);
+    }
+}
