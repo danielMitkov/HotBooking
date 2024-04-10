cd5c6d46ad33f3c76c1f3f28174e7efc4df470ea
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-10 23:02:22 +0300

    Create ReviewDetailsDto.cs

---
+++ HotBooking.Core/DTOs/ReviewDtos/ReviewDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿namespace HotBooking.Core.DTOs.ReviewDtos;
+
+public record ReviewDetailsDto(
+decimal Score,
+string Title,
+string Comment,
+DateTime ReviewedOnDate,
+DateTime CheckInDate,
+DateTime CheckOutDate,
+int AdultsCount,
+string RoomTitle
+);
