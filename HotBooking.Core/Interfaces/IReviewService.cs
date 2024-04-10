1a7ac218281d4fd217806a4e5cacb5326687e5ec
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-10 23:02:20 +0300

    Create IReviewService.cs

---
+++ HotBooking.Core/Interfaces/IReviewService.cs
@@ -0,0 +1,8 @@
+﻿using HotBooking.Core.DTOs.ReviewDtos;
+
+namespace HotBooking.Core.Interfaces;
+
+public interface IReviewService
+{
+    Task<BrowseReviewsOutputDto?> GetReviewsForHotel(BrowseReviewsInputDto inputDto);
+}
