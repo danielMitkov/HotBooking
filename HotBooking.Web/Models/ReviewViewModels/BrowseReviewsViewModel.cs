cc9869f1b4d7cc7830246f4077da69ea2e9f2b72
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-10 23:02:07 +0300

    Create BrowseReviewsViewModel.cs

---
+++ HotBooking.Web/Models/ReviewViewModels/BrowseReviewsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using HotBooking.Core.DTOs.ReviewDtos;
+
+namespace HotBooking.Web.Models.ReviewViewModels;
+
+public class BrowseReviewsViewModel
+{
+    public Guid HotelId { get; set; }
+    public int Page { get; set; } = 1;
+
+    
+    public int ReviewsCount { get; set; }
+
+    public PagerViewModel? Pager { get; set; }
+
+    public IEnumerable<ReviewDetailsDto> Reviews { get; set; } = new List<ReviewDetailsDto>();
+}
