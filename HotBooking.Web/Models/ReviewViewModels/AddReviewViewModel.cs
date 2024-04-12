a7c01868c7bf88f50258031c1d9a09d7bf34f5ba
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-12 22:25:13 +0300

    Create AddReviewViewModel.cs

---
+++ HotBooking.Web/Models/ReviewViewModels/AddReviewViewModel.cs
@@ -0,0 +1,24 @@
+﻿using HotBooking.Data.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotBooking.Web.Models.ReviewViewModels;
+
+public class AddReviewViewModel
+{
+    [Required]
+    public Guid HotelId { get; set; }
+
+    [Required]
+    [Range(ReviewConstants.RatingScoreMin, ReviewConstants.RatingScoreMax)]
+    public decimal RatingScore { get; set; }
+
+    [Required]
+    [MinLength(ReviewConstants.TitleLengthMin)]
+    [MaxLength(ReviewConstants.TitleLengthMax)]
+    public string Title { get; set; } = null!;
+
+    [Required]
+    [MinLength(ReviewConstants.CommentMin)]
+    [MaxLength(ReviewConstants.CommentMax)]
+    public string Comment { get; set; } = null!;
+}
