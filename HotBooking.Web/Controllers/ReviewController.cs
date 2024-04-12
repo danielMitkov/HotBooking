7a95827253da9291b1ce5968fac425b9ab9ffcd6
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-12 22:24:54 +0300

    added Add action

--- HotBooking.Web/Controllers/ReviewController.cs
+++ HotBooking.Web/Controllers/ReviewController.cs
@@ -1,10 +1,13 @@
 ﻿using HotBooking.Core.DTOs.ReviewDtos;
 using HotBooking.Core.Interfaces;
+using HotBooking.Web.Extensions;
 using HotBooking.Web.Models.ReviewViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotBooking.Web.Controllers;
 
+[Authorize]
 public class ReviewController : Controller
 {
     public const string Name = "Review";
@@ -16,10 +19,11 @@ public class ReviewController : Controller
         this.reviewService = reviewService;
     }
 
+    [AllowAnonymous]
     [Route("Review/All/{page?}")]
     public async Task<IActionResult> All(Guid hotelId, int page = 1)
     {
-        BrowseReviewsInputDto inputDto = new(hotelId, page, 2);
+        BrowseReviewsInputDto inputDto = new(User.GetUserId(), hotelId, page, 2);
 
         BrowseReviewsOutputDto? outputDto = await reviewService.GetReviewsForHotel(inputDto);
 
@@ -31,11 +35,42 @@ public class ReviewController : Controller
 
         BrowseReviewsViewModel model = new()
         {
+            HotelId = hotelId,
             Page = page,
             ReviewsCount = outputDto.ReviewsCount,
             Pager = new(outputDto.TotalPagesCount, page),
             Reviews = outputDto.Reviews
         };
+
+        return View(model);
+    }
+
+    public async Task<IActionResult> Add(Guid hotelId)
+    {
+        AddReviewViewModel model = new()
+        {
+            HotelId = hotelId
+        };
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(AddReviewViewModel model)
+    {
+        if (ModelState.IsValid == false)
+        {
+            return View(model);
+        }
+
+        AddReviewInputDto inputDto = new(
+            User.GetUserId(),
+            model.HotelId,
+            model.RatingScore,
+            model.Title,
+            model.Comment);
+
+        bool status = await reviewService.AddReviewAsync(inputDto);
 
         return View(model);
     }
