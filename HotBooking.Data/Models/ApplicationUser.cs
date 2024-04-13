f4be46339d59b61f7a33e28c3dd680be1c2c57c1
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-13 09:02:33 +0300

    removed PublicId

--- HotBooking.Data/Models/ApplicationUser.cs
+++ HotBooking.Data/Models/ApplicationUser.cs
@@ -6,9 +6,6 @@ namespace HotBooking.Data.Models;
 public class ApplicationUser : IdentityUser<int>
 {
     [Required]
-    public Guid PublicId { get; set; } = Guid.NewGuid();
-
-    [Required]
     public bool IsDeleted { get; set; } = false;
 
     public ICollection<Booking> Bookings { get; set; } = new HashSet<Booking>();
