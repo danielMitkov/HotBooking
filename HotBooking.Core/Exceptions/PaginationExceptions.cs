3a23c6df1d9cad65a34b53887f481ddd1e9e3573
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-17 01:57:51 +0300

    Create PaginationExceptions.cs

---
+++ HotBooking.Core/Exceptions/PaginationExceptions.cs
@@ -0,0 +1,9 @@
+﻿namespace HotBooking.Core.Exceptions;
+
+public class PageOutOfRangeException : InvalidModelDataException
+{
+    public PageOutOfRangeException(int maxPage)
+        : base(string.Empty, $"Page number must between 1 and {maxPage}")
+    {
+    }
+}
