8a1b2ab9642b12435d607b033387d8002cca5480
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-17 01:57:49 +0300

    Create InvalidModelDataException.cs

---
+++ HotBooking.Core/Exceptions/InvalidModelDataException.cs
@@ -0,0 +1,16 @@
+﻿namespace HotBooking.Core.Exceptions;
+
+public class InvalidModelDataException : Exception
+{
+    public string PropertyName { get; protected set; }
+
+    public InvalidModelDataException(string message) : base(message)
+    {
+        PropertyName = string.Empty;
+    }
+
+    public InvalidModelDataException(string propertyName, string message) : base(message)
+    {
+        PropertyName = propertyName;
+    }
+}
