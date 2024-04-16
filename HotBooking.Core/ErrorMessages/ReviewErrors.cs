ecaa5488a0456cd2dc48a8cbb3c471fdc4bf2cf6
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-17 01:57:43 +0300

    Create ReviewErrors.cs

---
+++ HotBooking.Core/ErrorMessages/ReviewErrors.cs
@@ -0,0 +1,9 @@
+﻿namespace HotBooking.Core.ErrorMessages;
+
+public static class ReviewErrors
+{
+    public const string NotFound = "The Review is not found!";
+    public const string AlreadyHasReview = "You already have a review!";
+    public const string DoesNotHaveBooking = "You don't have a booking to review!";
+    public const string NotTheAuthorOfReview = "You are not the author of this review!";
+}
