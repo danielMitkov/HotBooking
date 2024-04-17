1dee4abfd4bd24e332ae1980d9c0cbdb073882a2
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-17 23:29:25 +0300

    Create UserDetailsDto.cs

---
+++ HotBooking.Core.Models/DTOs/UserDtos/UserDetailsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HotBooking.Core.Models.DTOs.UserDtos;
+public class UserDetailsDto
+{
+    public string UserName { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+}
