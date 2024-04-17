440e7c6fb12083ae72595c412752c78429065e5f
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-17 19:43:39 +0300

    Create IBookingService.cs

---
+++ HotBooking.Core/Interfaces/IBookingService.cs
@@ -0,0 +1,10 @@
+﻿using HotBooking.Core.Models.DTOs.BookingDtos;
+
+namespace HotBooking.Core.Interfaces;
+
+public interface IBookingService
+{
+    Task AddAsync(BookingAddDto addDto);
+    Task<ICollection<BookingListDto>> GetBookingsAsync(int userId);
+    Task CancelAsync(Guid id);
+}
