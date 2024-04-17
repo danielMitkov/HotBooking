e4fb004cb7da7c6b85ce3738969be871ef66e198
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-17 14:54:34 +0300

    Create CartController.cs

---
+++ HotBooking.Web/Controllers/CartController.cs
@@ -0,0 +1,47 @@
+﻿using HotBooking.Core.Exceptions;
+using HotBooking.Core.Interfaces;
+using HotBooking.Core.Models.DTOs.CartDtos;
+using HotBooking.Web.Extensions;
+using HotBooking.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotBooking.Web.Controllers;
+
+public class CartController : Controller
+{
+    public const string Name = "Cart";
+
+    private readonly ILogger<CartController> logger;
+    private readonly ICartService cartService;
+
+    public CartController(
+        ILogger<CartController> logger,
+        ICartService cartService)
+    {
+        this.logger = logger;
+        this.cartService = cartService;
+    }
+
+    [Route("Cart/Add/{roomId}")]
+    public async Task<IActionResult> Add(Guid roomId, SearchHotelsViewModel search)
+    {
+        var addDto = new CartAddDto(
+            User.GetId(),
+            roomId,
+            search.CheckInDate,
+            search.CheckOutDate,
+            search.AdultsCount);
+
+        try
+        {
+            await cartService.AddAsync(addDto);
+            return View();
+        }
+        catch (InvalidModelDataException ex)
+        {
+            logger.LogWarning(ex, DateTime.Now.ToString());
+            TempData["Error"] = ex.Message;
+            return RedirectToAction(nameof(HotelsController.List), HotelsController.Name);
+        }
+    }
+}
