3dd7e92ca38ab3336c787a718d03ba813c594e39
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-17 23:29:23 +0300

    Create IUserService.cs

---
+++ HotBooking.Core/Interfaces/IUserService.cs
@@ -0,0 +1,8 @@
+﻿using HotBooking.Core.Models.DTOs.UserDtos;
+
+namespace HotBooking.Core.Interfaces;
+public interface IUserService
+{
+    Task<UserDetailsDto?> GetDetailsAsync(int id);
+    Task EditAsync(int id, UserDetailsDto detailsDto);
+}
