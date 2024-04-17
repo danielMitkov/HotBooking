ed54abc7215f9f117ec389518b1a39e16f24191c
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-17 23:29:21 +0300

    Create UsersController.cs

---
+++ HotBooking.Web/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using HotBooking.Core.Interfaces;
+using HotBooking.Core.Models.DTOs.UserDtos;
+using HotBooking.Web.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotBooking.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IUserService userService;
+
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public async Task<IActionResult> Details()
+        {
+            var user = await userService.GetDetailsAsync(User.GetId());
+
+            return View(user);
+        }
+
+        public async Task<IActionResult> Edit()
+        {
+            var user = await userService.GetDetailsAsync(User.GetId());
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UserDetailsDto applicationUser)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View(applicationUser);
+            }
+
+            try
+            {
+                await userService.EditAsync(User.GetId(), applicationUser);
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                return View(applicationUser);
+            }
+        }
+    }
+}
