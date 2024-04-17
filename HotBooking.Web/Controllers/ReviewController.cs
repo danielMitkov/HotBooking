e64c88fe54109264e1f8c8117b4db12c2886089d
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-17 05:24:44 +0300

    added edit, add and delete

--- HotBooking.Web/Controllers/ReviewController.cs
+++ HotBooking.Web/Controllers/ReviewController.cs
@@ -1,5 +1,6 @@
-﻿using HotBooking.Core.DTOs.ReviewDtos;
+﻿using HotBooking.Core.Exceptions;
 using HotBooking.Core.Interfaces;
+using HotBooking.Core.Models.DTOs.ReviewDtos;
 using HotBooking.Web.Extensions;
 using HotBooking.Web.Models.ReviewViewModels;
 using Microsoft.AspNetCore.Authorization;
@@ -12,66 +13,148 @@ public class ReviewController : Controller
 {
     public const string Name = "Review";
 
+    private readonly ILogger<ReviewController> logger;
     private readonly IReviewService reviewService;
 
-    public ReviewController(IReviewService reviewService)
+    public ReviewController(ILogger<ReviewController> logger,
+        IReviewService reviewService)
     {
+        this.logger = logger;
         this.reviewService = reviewService;
     }
 
     [AllowAnonymous]
     [Route("Review/All/{page?}")]
-    public async Task<IActionResult> All(Guid hotelId, int page = 1)
+    public async Task<IActionResult> All(Guid hotelPublicId, int page = 1)
     {
-        BrowseReviewsInputDto inputDto = new(User.GetId(), hotelId, page, 2);
+        var inputDto = new BrowseReviewsInputDto(User.GetId(), hotelPublicId, page, 2);
 
-        BrowseReviewsOutputDto? outputDto = await reviewService.GetReviewsForHotel(inputDto);
-
-        if (outputDto == null)
+        try
         {
-            TempData["Error"] = "Hotel Not Found!";
-            return BadRequest();
+            var outputDto = await reviewService.GetReviewsForHotelAsync(inputDto);
+
+            var browseModel = new ReviewBrowseViewModel()
+            {
+                HotelPublicId = hotelPublicId,
+                Page = page,
+                ReviewsCount = outputDto.ReviewsCount,
+                Pager = new(outputDto.TotalPagesCount, page),
+                CanAddReview = outputDto.CanAddReview,
+                Reviews = outputDto.Reviews
+            };
+
+            return base.View(browseModel);
         }
-
-        BrowseReviewsViewModel model = new()
+        catch (InvalidModelDataException ex)
         {
-            HotelId = hotelId,
-            Page = page,
-            ReviewsCount = outputDto.ReviewsCount,
-            Pager = new(outputDto.TotalPagesCount, page),
-            Reviews = outputDto.Reviews
-        };
-
-        return View(model);
+            logger.LogWarning(ex, DateTime.Now.ToString());
+            TempData["Error"] = ex.Message;
+            return base.RedirectToAction(nameof(HomeController.Index), HomeController.Name);
+        }
     }
 
-    public IActionResult Add(Guid hotelId)
+    public IActionResult Add(Guid hotelPublicId)
     {
-        FormReviewViewModel model = new()
+        var addModel = new ReviewAddViewModel()
         {
-            HotelId = hotelId
+            HotelPublicId = hotelPublicId
         };
 
-        return View(model);
+        return View(addModel);
     }
 
     [HttpPost]
-    public async Task<IActionResult> Add(FormReviewViewModel model)
+    public async Task<IActionResult> Add(ReviewAddViewModel addModel)
     {
         if (ModelState.IsValid == false)
         {
-            return View(model);
+            return View(addModel);
         }
 
-        AddReviewInputDto inputDto = new(
+        var inputDto = new ReviewAddDto(
             User.GetId(),
-            model.HotelId,
-            model.RatingScore,
-            model.Title,
-            model.Comment);
+            addModel.HotelPublicId,
+            addModel.RatingScore,
+            addModel.Title,
+            addModel.Comment);
 
-        bool status = await reviewService.AddReviewAsync(inputDto);
+        try
+        {
+            await reviewService.AddReviewAsync(inputDto);
+        }
+        catch (InvalidModelDataException ex)
+        {
+            logger.LogWarning(ex, DateTime.Now.ToString());
+            ModelState.AddModelError(ex.PropertyName, ex.Message);
+        }
 
-        return View(model);
+        TempData["OK"] = "Review is Added!";
+        return RedirectToAction(nameof(All), new { hotelPublicId = addModel.HotelPublicId });
+    }
+
+    public async Task<IActionResult> Edit(Guid reviewPublicId, Guid hotelPublicId)
+    {
+        try
+        {
+            var reviewDto = await reviewService.GetReviewAsync(reviewPublicId);
+
+            var editModel = new ReviewEditViewModel()
+            {
+                ReviewPublicId = reviewPublicId,
+                HotelPublicId = hotelPublicId,
+                RatingScore = reviewDto.RatingScore,
+                Title = reviewDto.Title,
+                Comment = reviewDto.Comment
+            };
+
+            return base.View(editModel);
+        }
+        catch (Core.Exceptions.InvalidModelDataException ex)
+        {
+            logger.LogWarning(ex, DateTime.Now.ToString());
+            TempData["Error"] = ex.Message;
+            return base.RedirectToAction(nameof(All), new { hotelPublicId });
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(ReviewEditViewModel editModel)
+    {
+        var inputDto = new ReviewEditDto(
+            User.GetId(),
+            editModel.ReviewPublicId,
+            editModel.RatingScore,
+            editModel.Title,
+            editModel.Comment);
+
+        try
+        {
+            await reviewService.EditAsync(inputDto);
+        }
+        catch (InvalidModelDataException ex)
+        {
+            logger.LogWarning(ex, DateTime.Now.ToString());
+            ModelState.AddModelError(ex.PropertyName, ex.Message);
+            return base.View(editModel);
+        }
+
+        TempData["OK"] = "Review is Edited!";
+        return RedirectToAction(nameof(All), new { hotelPublicId = editModel.HotelPublicId });
+    }
+
+    public async Task<IActionResult> Delete(Guid reviewPublicId, Guid hotelPublicId)
+    {
+        try
+        {
+            await reviewService.DeleteAsync(reviewPublicId, User.GetId());
+        }
+        catch (InvalidModelDataException ex)
+        {
+            logger.LogWarning(ex, DateTime.Now.ToString());
+            TempData["Error"] = ex.Message;
+        }
+
+        TempData["OK"] = "Review is Deleted!";
+        return RedirectToAction(nameof(All), new { hotelPublicId });
     }
 }
