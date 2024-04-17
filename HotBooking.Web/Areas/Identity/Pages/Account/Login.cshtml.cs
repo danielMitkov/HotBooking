16bf96f88d20d5e558f91d5fd6be74891d27dee2
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-17 05:19:56 +0300

    removed redundant usings

--- HotBooking.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ HotBooking.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -4,12 +4,10 @@
 
 using HotBooking.Data.Models;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
-using System.Security.Claims;
 
 namespace HotBooking.Web.Areas.Identity.Pages.Account
 {
