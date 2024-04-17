b95a4a8c8c1b85d45c40e40217c60be5219d0ad5
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-17 14:54:30 +0300

    Create Cart.cs

---
+++ HotBooking.Data/Models/Cart.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotBooking.Data.Models;
+
+public class Cart
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public Guid PublicId { get; set; }
+
+    [Required]
+    public bool IsActive { get; set; } = true;
+
+    [Required]
+    public int UserId { get; set; }
+
+    [ForeignKey(nameof(UserId))]
+    public ApplicationUser User { get; set; } = null!;
+
+    public ICollection<Booking> Bookings { get; set; } = new HashSet<Booking>();
+}
