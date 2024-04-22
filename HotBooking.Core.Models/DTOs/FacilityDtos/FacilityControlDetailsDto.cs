32445ec072c5034fc19bad04980b986c150d2fb1
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-22 10:38:12 +0300

    Create FacilityControlDetailsDto.cs

---
+++ HotBooking.Core.Models/DTOs/FacilityDtos/FacilityControlDetailsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HotBooking.Core.Models.DTOs.FacilityDtos;
+
+public record FacilityControlDetailsDto(
+Guid PublicId,
+bool IsActive,
+string Name,
+DateTime CreatedOn,
+string SvgTag
+);
