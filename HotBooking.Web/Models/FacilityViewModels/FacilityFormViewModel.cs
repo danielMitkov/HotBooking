52098ec14078526a5688b5045e915a78165c5a20
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-24 00:12:03 +0300

    Create FacilityFormViewModel.cs

---
+++ HotBooking.Web/Models/FacilityViewModels/FacilityFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using HotBooking.Data.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotBooking.Web.Models.FacilityViewModels;
+
+public class FacilityFormViewModel
+{
+    [Required]
+    public Guid PublicId { get; set; }
+
+    [Required]
+    [MinLength(FacilityConstants.NameLengthMin)]
+    [MaxLength(FacilityConstants.NameLengthMax)]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    [MinLength(FacilityConstants.SvgTagLengthMin)]
+    [MaxLength(FacilityConstants.SvgTagLengthMax)]
+    public string SvgTag { get; set; } = null!;
+}
