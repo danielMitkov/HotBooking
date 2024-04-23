bde1dac94330ac0252c5d670f5969efa1089c324
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-23 23:20:10 +0300

    Create ManagerService.cs

---
+++ HotBooking.Core/Services/ManagerService.cs
@@ -0,0 +1,23 @@
+﻿using HotBooking.Core.Interfaces;
+using HotBooking.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotBooking.Core.Services;
+
+public class ManagerService : IManagerService
+{
+    private readonly HotBookingDbContext dbContext;
+
+    public ManagerService(HotBookingDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task<bool> DoesManagerExistAsync(int userId)
+    {
+        bool isManagerFound = await dbContext.Managers
+            .AnyAsync(m => m.UserId == userId);
+
+        return isManagerFound;
+    }
+}
