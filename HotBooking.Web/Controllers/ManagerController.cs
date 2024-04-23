928f039dc3fb36a1e632169da0b7309c47dcb0dc
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-23 23:19:35 +0300

    Create ManagerController.cs

---
+++ HotBooking.Web/Controllers/ManagerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotBooking.Web.Controllers;
+public class ManagerController : BaseController
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> Become()
+    {
+        return View();
+    }
+}
