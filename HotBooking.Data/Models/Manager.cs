47906c0a95c53bbe6c1edd848bfb6a18271f56e9
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-23 23:19:27 +0300

    Create Manager.cs

---
+++ HotBooking.Data/Models/Manager.cs
@@ -0,0 +1,43 @@
+﻿using HotBooking.Data.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotBooking.Data.Models;
+
+public class Manager : IPublicId
+{
+    public Manager()
+    {
+        PublicId = Guid.NewGuid();
+        IsActive = true;
+
+        Hotels = new HashSet<Hotel>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public Guid PublicId { get; set; }
+
+    [Required]
+    public bool IsActive { get; set; }
+
+    [Required]
+    public string CompanyName { get; set; } = null!;
+
+    [Required]
+    public string Department { get; set; } = null!;
+
+    [Required]
+    public string PhoneNumber { get; set; } = null!;
+
+    [Required]
+    public int UserId { get; set; }
+
+    [Required]
+    [ForeignKey(nameof(UserId))]
+    public ApplicationUser User { get; set; } = null!;
+
+    public ICollection<Hotel> Hotels { get; set; }
+}
