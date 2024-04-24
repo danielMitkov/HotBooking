e27acacd3f5ada48d9c93ded2a0fb32284ecb25e
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-24 14:07:41 +0300

    Create ManagerFormViewModel.cs

---
+++ HotBooking.Web/Models/ManagerViewModels/ManagerFormViewModel.cs
@@ -0,0 +1,23 @@
+﻿using HotBooking.Data.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotBooking.Web.Models.ManagerViewModels;
+
+public class ManagerFormViewModel
+{
+    [Required]
+    [MinLength(ManagerConstants.CompanyNameLengthMin)]
+    [MaxLength(ManagerConstants.CompanyNameLengthMax)]
+    public string CompanyName { get; set; } = null!;
+
+    [Required]
+    [MinLength(ManagerConstants.DepartmentLengthMin)]
+    [MaxLength(ManagerConstants.DepartmentLengthMax)]
+    public string Department { get; set; } = null!;
+
+    [Required]
+    [MinLength(ManagerConstants.PhoneNumberLengthMin)]
+    [MaxLength(ManagerConstants.PhoneNumberLengthMax)]
+    [Phone]
+    public string PhoneNumber { get; set; } = null!;
+}
