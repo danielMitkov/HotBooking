3d567a43a5edc9523a20a956daf761dc06b034f7
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-24 23:58:11 +0300

    Create ManagerHotelPreviewDto.cs

---
+++ HotBooking.Core.Models/DTOs/ManagerDtos/ManagerHotelPreviewDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HotBooking.Core.Models.DTOs.ManagerDtos;
+
+public record ManagerHotelPreviewDto(
+Guid PublicId,
+string Name,
+string StreetAddress,
+string CityName,
+string CountryName,
+int StarRating
+);
