b581eae2cb0ab032b753c8666cf7e5bb3ee13840
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-25 00:01:15 +0300

    added constants

--- HotBooking.Data/Constants/ManagerConstants.cs
+++ HotBooking.Data/Constants/ManagerConstants.cs
@@ -1,6 +1,12 @@
 ﻿namespace HotBooking.Data.Constants;
 public static class ManagerConstants
 {
-    public const int PhoneNumberLengthMin = 1;
+    public const int CompanyNameLengthMin = 5;
+    public const int CompanyNameLengthMax = 50;
+
+    public const int DepartmentLengthMin = 5;
+    public const int DepartmentLengthMax = 50;
+
+    public const int PhoneNumberLengthMin = 10;
     public const int PhoneNumberLengthMax = 20;
 }
