5860212f699cd556c8ea1bedc3c4221677089f93
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-24 23:58:46 +0300

    added errors

--- HotBooking.Core/ErrorMessages/ManagerErrors.cs
+++ HotBooking.Core/ErrorMessages/ManagerErrors.cs
@@ -2,6 +2,8 @@
 
 public static class ManagerErrors
 {
+    public const string NotFound = "The Manager does not exist!";
     public const string AlreadyManager = "You are already a manager!";
     public const string PhoneNumberAlreadyExists = "There is already a manager with same phone number!";
+    public const string NotTheHotelManager = "You are not the manager of this hotel!";
 }
