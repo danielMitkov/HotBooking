9710adf2b19ce8ee82bbea29d6669c19abc28276
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-24 23:58:09 +0300

    Create HotelEditDto.cs

---
+++ HotBooking.Core.Models/DTOs/HotelDtos/HotelEditDto.cs
@@ -0,0 +1,13 @@
+﻿namespace HotBooking.Core.Models.DTOs.HotelDtos;
+
+public record HotelEditDto(
+Guid PublicId,
+string HotelName,
+string Description,
+string StreetAddress,
+string CityName,
+string CountryName,
+int StarRating,
+IEnumerable<Guid> SelectedFacilityIds,
+string ImageUrls
+);
