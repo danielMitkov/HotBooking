d6330d4a969564a4bc37a1846de3fc747d07f3fe
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-24 14:07:35 +0300

    Create ManagerErrors.cs

---
+++ HotBooking.Core/ErrorMessages/ManagerErrors.cs
@@ -0,0 +1,7 @@
+﻿namespace HotBooking.Core.ErrorMessages;
+
+public static class ManagerErrors
+{
+    public const string AlreadyManager = "You are already a manager!";
+    public const string PhoneNumberAlreadyExists = "There is already a manager with same phone number!";
+}
