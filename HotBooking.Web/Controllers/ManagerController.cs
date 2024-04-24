9ebd7996271584349bec66a3305779a5f81df710
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-25 00:02:27 +0300

    added MyHotels and Become

--- HotBooking.Web/Controllers/ManagerController.cs
+++ HotBooking.Web/Controllers/ManagerController.cs
@@ -1,15 +1,82 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotBooking.Core.Exceptions;
+using HotBooking.Core.Interfaces;
+using HotBooking.Core.Models.DTOs.ManagerDtos;
+using HotBooking.Web.Extensions;
+using HotBooking.Web.Models.ManagerViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HotBooking.Web.Controllers;
+
 public class ManagerController : BaseController
 {
-    public IActionResult Index()
+    public const string Name = "Manager";
+
+    private readonly IManagerService managerService;
+    private readonly ILogger<ManagerController> logger;
+
+    public ManagerController(
+        IManagerService managerService,
+        ILogger<ManagerController> logger)
     {
-        return View();
+        this.managerService = managerService;
+        this.logger = logger;
     }
 
-    public async Task<IActionResult> Become()
+    public async Task<IActionResult> MyHotels()
     {
-        return View();
+        try
+        {
+            var hotelDtos = await managerService.MyHotelsAsync(User.GetId());
+
+            return View(hotelDtos);
+        }
+        catch (InvalidModelDataException ex)
+        {
+            logger.LogWarning(ex, DateTime.Now.ToString());
+
+            TempData["Error"] = ex.Message;
+
+            return RedirectToAction(
+                nameof(HomeController.Index),
+                HomeController.Name);
+        }
+    }
+
+    public IActionResult Become()
+    {
+        var formModel = new ManagerFormViewModel();
+
+        return View(formModel);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Become(ManagerFormViewModel formModel)
+    {
+        if (ModelState.IsValid == false)
+        {
+            return View(formModel);
+        }
+
+        var formDto = new ManagerFormDto(
+            formModel.CompanyName,
+            formModel.Department,
+            formModel.PhoneNumber);
+
+        try
+        {
+            await managerService.BecomeAsync(User.GetId(), formDto);
+
+            return RedirectToAction(nameof(MyHotels));
+        }
+        catch (InvalidModelDataException ex)
+        {
+            logger.LogWarning(ex, DateTime.Now.ToString());
+
+            TempData["Error"] = ex.Message;
+
+            return RedirectToAction(
+                nameof(HomeController.Index),
+                HomeController.Name);
+        }
     }
 }
