ab819a0d37210879a33059a59b10098cd75ab97f
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-28 09:42:04 +0300

    added AllUsersDetailsAsync, MakeAdminAsync, MakeNormalAsync

--- HotBooking.Core/Interfaces/IUserService.cs
+++ HotBooking.Core/Interfaces/IUserService.cs
@@ -3,6 +3,9 @@
 namespace HotBooking.Core.Interfaces;
 public interface IUserService
 {
+    Task<ICollection<UserRoleDetailsDto>> AllUsersDetailsAsync();
+    Task MakeAdminAsync(int userId);
+    Task MakeNormalAsync(int userId);
     Task<UserDetailsDto?> GetDetailsAsync(int id);
     Task EditAsync(int id, UserDetailsDto detailsDto);
 }
--- HotBooking.Core/Services/UserService.cs
+++ HotBooking.Core/Services/UserService.cs
@@ -1,6 +1,11 @@
-﻿using HotBooking.Core.Interfaces;
+﻿using HotBooking.Core.ErrorMessages;
+using HotBooking.Core.Exceptions;
+using HotBooking.Core.Interfaces;
 using HotBooking.Core.Models.DTOs.UserDtos;
 using HotBooking.Data;
+using HotBooking.Data.Models;
+using HotBooking.Web.Constants;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotBooking.Core.Services;
@@ -8,10 +13,77 @@ namespace HotBooking.Core.Services;
 public class UserService : IUserService
 {
     private readonly HotBookingDbContext dbContext;
+    private readonly UserManager<ApplicationUser> userManager;
 
-    public UserService(HotBookingDbContext dbContext)
+    public UserService(HotBookingDbContext dbContext,
+        UserManager<ApplicationUser> userManager)
     {
         this.dbContext = dbContext;
+        this.userManager = userManager;
+    }
+
+    public async Task<ICollection<UserRoleDetailsDto>> AllUsersDetailsAsync()
+    {
+        var users = await dbContext.Users
+            .ToListAsync();
+
+        var usersDtos = users
+            .Select(u => new UserRoleDetailsDto()
+            {
+                Id = u.Id,
+                UserName = u.UserName,
+                Email = u.Email,
+                PhoneNumber = u.PhoneNumber,
+                isAdmin = false
+            })
+            .ToList();
+
+        for (int i = 0; i < users.Count; ++i)
+        {
+            usersDtos[i].isAdmin = await userManager.IsInRoleAsync(users[i], AdminConstants.AdminRoleName);
+        }
+
+        return usersDtos;
+    }
+
+    public async Task MakeAdminAsync(int userId)
+    {
+        var user = await dbContext.Users
+            .SingleOrDefaultAsync(u => u.Id == userId);
+
+        if (user == null)
+        {
+            throw new InvalidModelDataException(UserErrors.NotFound);
+        }
+
+        bool isUserAdmin = await userManager.IsInRoleAsync(user, AdminConstants.AdminRoleName);
+
+        if (isUserAdmin == true)
+        {
+            throw new InvalidModelDataException(UserErrors.AlreadyAdmin);
+        }
+
+        await userManager.AddToRoleAsync(user, AdminConstants.AdminRoleName);
+    }
+
+    public async Task MakeNormalAsync(int userId)
+    {
+        var user = await dbContext.Users
+            .SingleOrDefaultAsync(u => u.Id == userId);
+
+        if (user == null)
+        {
+            throw new InvalidModelDataException(UserErrors.NotFound);
+        }
+
+        bool isUserAdmin = await userManager.IsInRoleAsync(user, AdminConstants.AdminRoleName);
+
+        if (isUserAdmin == false)
+        {
+            throw new InvalidModelDataException(UserErrors.AlreadyNormal);
+        }
+
+        await userManager.RemoveFromRoleAsync(user, AdminConstants.AdminRoleName);
     }
 
     public async Task<UserDetailsDto?> GetDetailsAsync(int id)
