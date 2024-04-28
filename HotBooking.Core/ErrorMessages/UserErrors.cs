07f5d646dec7b322f90c2959654a6fc135dc0f3f
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-28 09:42:25 +0300

    added already Admin and already Normal

--- HotBooking.Core/ErrorMessages/UserErrors.cs
+++ HotBooking.Core/ErrorMessages/UserErrors.cs
@@ -3,4 +3,6 @@
 public static class UserErrors
 {
     public const string NotFound = "User Not Found!";
+    public const string AlreadyAdmin = "User is Already Admin!";
+    public const string AlreadyNormal = "User is Already a Normal user!";
 }
