2d2a97305bab3d140ff57e7529b7dfbda1766f09
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-28 03:42:20 +0300

    Create IFeatureService.cs

---
+++ HotBooking.Core/Interfaces/IFeatureService.cs
@@ -0,0 +1,8 @@
+﻿using HotBooking.Core.Models.DTOs.FeatureDtos;
+
+namespace HotBooking.Core.Interfaces;
+
+public interface IFeatureService
+{
+    Task<ICollection<FeatureChecksDto>> GetFeatureCheckboxesAsync(IEnumerable<Guid> selectedFeatureIds);
+}
