8bad51f96f58291554f04e03b6c84fe4ebed2e16
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-28 09:40:14 +0300

    Create UserRoleDetailsDto.cs

---
+++ HotBooking.Core.Models/DTOs/UserDtos/UserRoleDetailsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace HotBooking.Core.Models.DTOs.UserDtos;
+
+public class UserRoleDetailsDto
+{
+    public int Id { get; set; }
+    public string UserName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public bool isAdmin { get; set; }
+}
+
