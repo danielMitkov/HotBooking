68b626acd82f5ee6e6189a781a6eb9034896c472
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-28 03:43:14 +0300

    added NotManager

--- HotBooking.Core/ErrorMessages/HotelErrors.cs
+++ HotBooking.Core/ErrorMessages/HotelErrors.cs
@@ -3,4 +3,5 @@ public static class HotelErrors
 {
     public const string CityNotFound = "City Not Found!";
     public const string NotFound = "Hotel Not Found!";
+    public const string NotManager = "You are not the manager of this hotel!";
 }
