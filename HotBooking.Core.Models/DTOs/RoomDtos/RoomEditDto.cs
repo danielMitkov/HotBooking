1b7d764f5eddd905e7fbc7ceaf2d7fbcfeca28a8
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-28 03:42:16 +0300

    Create RoomEditDto.cs

---
+++ HotBooking.Core.Models/DTOs/RoomDtos/RoomEditDto.cs
@@ -0,0 +1,11 @@
+﻿namespace HotBooking.Core.Models.DTOs.RoomDtos;
+
+public record RoomEditDto(
+Guid PublicId,
+string Title,
+string Description,
+int BedsCount,
+int RoomSizeSquareMeters,
+decimal PricePerNight,
+IEnumerable<Guid> SelectedFeatureIds,
+string ImageUrls);
