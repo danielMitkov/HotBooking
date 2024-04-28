03836c8b3726c63367192f4d4b8d9ea189f9df7e
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-28 03:42:23 +0300

    Create IRoomService.cs

---
+++ HotBooking.Core/Interfaces/IRoomService.cs
@@ -0,0 +1,11 @@
+﻿using HotBooking.Core.Models.DTOs.RoomDtos;
+
+namespace HotBooking.Core.Interfaces;
+
+public interface IRoomService
+{
+    Task AddAsync(int userId, Guid hotelPublicId, RoomAddDto addDto);
+    Task<RoomEditDto> GetForEditAsync(int userId, Guid roomId);
+    Task<Guid> EditAsync(int userId, RoomEditDto editDto);
+    Task<Guid> DeleteAsync(int userId, Guid roomId);
+}
