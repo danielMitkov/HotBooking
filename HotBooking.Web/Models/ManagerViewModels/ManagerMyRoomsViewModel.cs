a847437d5fc4fcedad151e0b653557cfa8eac54f
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-28 03:42:35 +0300

    Create ManagerMyRoomsViewModel.cs

---
+++ HotBooking.Web/Models/ManagerViewModels/ManagerMyRoomsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using HotBooking.Core.Models.DTOs.ManagerDtos;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotBooking.Web.Models.ManagerViewModels;
+
+public class ManagerMyRoomsViewModel
+{
+    [Required]
+    public Guid HotelId { get; set; }
+
+    public ICollection<ManagerRoomPreviewDto> Rooms { get; set; }
+    = new List<ManagerRoomPreviewDto>();
+}
