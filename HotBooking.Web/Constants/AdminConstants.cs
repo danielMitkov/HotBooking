c2c03a7b28a1d8761628344133cd7adc3558b88a
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-28 09:40:44 +0300

    Delete AdminConstants.cs

--- HotBooking.Web/Constants/AdminConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace HotBooking.Web.Constants;
-
-public static class AdminConstants
-{
-    public const string AdminAreaName = "Admin";
-    public const string AdminRoleName = "Administrator";
-}
