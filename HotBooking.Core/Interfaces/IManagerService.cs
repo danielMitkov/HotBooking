f1e95cb909c0f3ef77b4d30a5f3d82f5c856ebf6
danielMitkov <daniel.mitkov01@gmail.com>
2024-04-28 03:43:49 +0300

    added MyRoomsAsync

--- HotBooking.Core/Interfaces/IManagerService.cs
+++ HotBooking.Core/Interfaces/IManagerService.cs
@@ -7,4 +7,5 @@ public interface IManagerService
     Task<bool> DoesManagerExistAsync(int userId);
     Task BecomeAsync(int userId, ManagerFormDto formDto);
     Task<ICollection<ManagerHotelPreviewDto>> MyHotelsAsync(int userId);
+    Task<ICollection<ManagerRoomPreviewDto>> MyRoomsAsync(int userId, Guid hotelId);
 }
