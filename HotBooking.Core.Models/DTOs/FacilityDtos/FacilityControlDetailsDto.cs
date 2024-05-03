d294a7cb389c104e120baf8e927f13eae1999cc0
danielMitkov <daniel.mitkov01@gmail.com>
2024-05-03 13:59:34 +0300

    Delete FacilityControlDetailsDto.cs

--- HotBooking.Core.Models/DTOs/FacilityDtos/FacilityControlDetailsDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HotBooking.Core.Models.DTOs.FacilityDtos;
-
-public record FacilityControlDetailsDto(
-Guid PublicId,
-bool IsActive,
-string Name,
-DateTime CreatedOn,
-string SvgTag
-);
