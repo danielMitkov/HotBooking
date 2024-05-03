aa11c43defd9c6ce714729cdb9e7f1a14f71ab28
danielMitkov <daniel.mitkov01@gmail.com>
2024-05-03 16:34:07 +0300

    added AlreadyDeleted

--- HotBooking.Core/ErrorMessages/FacilityErrors.cs
+++ HotBooking.Core/ErrorMessages/FacilityErrors.cs
@@ -4,4 +4,5 @@ public static class FacilityErrors
 {
     public const string NotFound = "Facility Not Found!";
     public const string NameAlreadyExists = "Facility Name Already Exists!";
+    public const string AlreadyDeleted = "Facility is Already Deleted!";
 }
