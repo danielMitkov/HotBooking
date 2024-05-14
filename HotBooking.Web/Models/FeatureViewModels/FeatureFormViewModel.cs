535c1846fd1bfe871b6df0a77d7346e7ad0bf003
danielMitkov <daniel.mitkov01@gmail.com>
2024-05-14 16:18:27 +0300

    Create FeatureFormViewModel.cs

---
+++ HotBooking.Web/Models/FeatureViewModels/FeatureFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using HotBooking.Data.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotBooking.Web.Models.FeatureViewModels;
+
+public class FeatureFormViewModel
+{
+    [Required]
+    public Guid PublicId { get; set; }
+
+    [Required]
+    [MinLength(FeatureConstants.NameLengthMin)]
+    [MaxLength(FeatureConstants.NameLengthMax)]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    [MinLength(FeatureConstants.SvgTagLengthMin)]
+    [MaxLength(FeatureConstants.SvgTagLengthMax)]
+    public string SvgTag { get; set; } = null!;
+}
