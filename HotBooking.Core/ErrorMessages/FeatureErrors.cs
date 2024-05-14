f46c01b0df00dc7799b00b9b164098cbf4c28330
danielMitkov <daniel.mitkov01@gmail.com>
2024-05-14 15:48:30 +0300

    Create FeatureErrors.cs

---
+++ HotBooking.Core/ErrorMessages/FeatureErrors.cs
@@ -0,0 +1,8 @@
+﻿namespace HotBooking.Core.ErrorMessages;
+
+public static class FeatureErrors
+{
+    public const string NotFound = "Feature Not Found!";
+    public const string NameAlreadyExists = "Feature Name Already Exists!";
+    public const string AlreadyDeleted = "Feature is Already Deleted!";
+}
